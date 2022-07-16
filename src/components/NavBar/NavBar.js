de5877bce6f04f9d9bd7246574a53231e0edf3fb
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2022-07-16 09:47:35 -0500

    desafio 2 menu ecommerce

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
